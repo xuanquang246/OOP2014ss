406ea5e163331399c83868c9bb95597fb3a0ff59
xuanquang246 <quang.clc246@gmail.com>
2014-07-04 23:49:09 +0700

    oop2014ss

---
+++ bai1/asg01.java
@@ -0,0 +1,17 @@
+package project1;
+import java.util.Scanner;
+
+public class asg01 {
+	public static void main(String[] args){
+		int count;
+		//Scanner input = new Scanner(System.in);
+		System.out.println("Ong Xuan Quang" + '\t' + "11020246" + '\t' + "xuanquang246" + '\t' + "quang.clc246@gmail.com.");
+		for(count = 9; count > 0; count--){
+			System.out.println(count + " bottles of beer on the wall, " + count + " bottles of beer.");
+			System.out.println("Take one down, pass it around,");
+		}
+		System.out.println("Take one down, pass it around,");
+		System.out.println("No more bottles of beer on the wall.");
+	}
+
+}
